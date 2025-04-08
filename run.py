c0df6db8be606731d364be42ad43cf4e4accb717
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2025-04-08 12:39:26 +0700

    initial commit

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from uvicorn import run
-import sys
-print(sys.argv)
-port = int(sys.argv[1])
-assert isinstance(port, int), "please provide port, for an example port of 8080, python3 run.py 8080"
-run(app, port=port)
